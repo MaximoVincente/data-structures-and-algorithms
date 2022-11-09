b2879cd6eccae79bae09953068d2308110cbbdfc
Maximo Vincente Mejia <maximovincente@gmail.com>
2022-11-08 20:28:42 -0500

    fixed bug in challenge

--- java/datastructures/lib/src/main/java/datastructures/queues/StackQueuePseudo.java
+++ java/datastructures/lib/src/main/java/datastructures/queues/StackQueuePseudo.java
@@ -4,8 +4,6 @@ import java.util.Stack;
 
 public class StackQueuePseudo {
 
-
-
   static Stack<Integer> stack1 = new Stack<>();
   static Stack<Integer> stack2 = new Stack<>();
 
@@ -13,19 +11,17 @@ public class StackQueuePseudo {
 
     while (!stack1.isEmpty()) {
       stack2.push(stack1.pop());
-
     }
+
     stack1.push(val);
+
     while (!stack2.isEmpty()) {
       stack1.push(stack2.pop());
     }
   }
   public int dequeue() {
-
     int top = stack1.peek();
     stack1.pop();
-
     return top;
   }
-
 }
