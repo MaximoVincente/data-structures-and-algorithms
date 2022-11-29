d938b1abb40faccfc51461c033fd52fe66645de8
Maximo Vincente Mejia <maximovincente@gmail.com>
2022-11-29 15:25:49 -0500

    added code and test

--- java/datastructures/lib/src/test/java/datastructures/sort/TestSort.java
+++ java/datastructures/lib/src/test/java/datastructures/sort/TestSort.java
@@ -1,6 +1,7 @@
 package datastructures.sort;
 
 import datastructures.sorts.Insertion;
+import datastructures.sorts.Merge;
 import org.junit.jupiter.api.Test;
 
 import javax.swing.*;
@@ -15,6 +16,15 @@ public class TestSort {
     int[] arr = {6, 2, 20, 35, 17, 14};
     int[] target = {2, 6, 14, 17, 20, 35};
     sut.selectionSort(arr);
+    assertArrayEquals(arr, target);
+  }
+
+  @Test
+  void testMergeSort() {
+    Merge sut = new Merge();
+    int[] arr = {5, 6, 3, 2, 9};
+    int[] target = {2, 3, 5, 6, 9};
+    sut.sort(arr, 0, arr.length - 1);
     assertArrayEquals(arr, target);
   }
 }
